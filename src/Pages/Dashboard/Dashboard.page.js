5e70980f6235e4e5a4bdb01c68381bc9511f173c
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2021-12-04 03:03:37 +0530

    breadcumb added

--- src/Pages/Dashboard/Dashboard.page.js
+++ src/Pages/Dashboard/Dashboard.page.js
@@ -2,10 +2,16 @@ import React from 'react'
 import { Container,Row,Col,Button } from 'react-bootstrap'
 import {TicketTabel} from '../../Components/Ticket-tabel/TicketTabel-comp'
 import tickets from '../../assets/data/dummy-tickets.json'
+import { PageBreadcrumb } from '../../Components/breadcumb/Breadcrumb.comp'
 export const Dashboard = () => {
     return (
         <div>
             <Container>
+                <Row>
+                    <Col>
+                    <PageBreadcrumb page="Dashboard"/>
+                    </Col>
+                </Row>
                 <Row>
                     <Col className="text-center mt-5 mb-2">
                     <Button  variant="info" style={{fontSize:'2rem',padding: '10px 30px'}}>Add New Ticket</Button>
